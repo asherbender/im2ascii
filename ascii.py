55f7d5c74a6ab5c26a20976bf7a60ac4ad3e3c35
Asher Bender <a.bender.dev@gmail.com>
2015-07-12 01:03:52 +1000

    scale_image: Function added for scaling images

--- ascii.py
+++ ascii.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python
 
+def scale_image(img, rescaled_width=80):
+
+    # Get size of image.
+    height, width = img.shape
+
+    # Calculate height of re-scaled image in pixels (preserving aspect ratio).
+    rescaled_height = int(float(height * rescaled_width) / float(width))
+
+    # Re-scale image.
+    return skimage.transform.resize(img,
+                                    (rescaled_height, rescaled_width),
+                                    clip=True)
 
 if __name__=="__main__":
