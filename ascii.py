b3c28058cfa05b0e14b11097d28029d17e3ce184
Asher Bender <a.bender.dev@gmail.com>
2015-07-12 01:16:16 +1000

    average_pixels: Calculating intensity in blocks
        - Calculate the image intensity in non-square blocks to account for
          rectangular pixels.

--- ascii.py
+++ ascii.py
@@ -13,4 +13,22 @@ def scale_image(img, rescaled_width=80):
                                     (rescaled_height, rescaled_width),
                                     clip=True)
 
+
+def average_pixels(img, char_width=1, char_height=2):
+
+    # Get size of image.
+    height, width = img.shape
+
+    # Calculate number of blocks, in rows and columns, to perform averaging.
+    rows = int(float(height) / float(char_height))
+    cols = int(float(width) / float(char_width))
+
+    # Average image in blocks.
+    mean_image = np.zeros((rows, cols))
+    for i in range(rows):
+        for j in range(cols):
+            mean_image[i, j] = img[(i*char_height):((i+1)*char_height),
+                                   (j*char_width):((j+1)*char_width)].mean()
+
+    return mean_image, rows, cols
 if __name__=="__main__":
