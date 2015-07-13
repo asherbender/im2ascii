2aa446e6fd955a8524b5bac66647d329c75b0c89
Asher Bender <a.bender.dev@gmail.com>
2015-07-13 23:38:28 +1000

    LICENSE: project to be released under GPLv3

--- ascii.py
+++ ascii.py
@@ -8,6 +8,20 @@ from skimage import io
 import skimage.transform
 from skimage.color import rgb2gray
 
+#    Copyright (C) 2015, Asher Bender
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Characters available for ASCII art.
 #
